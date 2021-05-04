6f4f082254ffd6115e2dfb2b4adf879a60df6fe1
Héctor Martel <hmartelb@hotmail.com>
2021-05-04 09:58:57 +0100

    Update scraper.py

--- data/scraper.py
+++ data/scraper.py
@@ -1,6 +1,4 @@
-'''
-https://gist.github.com/WalterSimoncini/defca6de456bb168ada303085358bf0a
-'''
+
 
 import json
 import time
@@ -10,7 +8,62 @@ import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
-def process_category(args):
+def process_9gag(args):
+    fetched_memes = []
+    errors = 0
+    # for i in tqdm(range(args.))
+    pass
+
+def process_me_dot_me(args):
+    pass
+
+def templates_imgflip(args):
+    args.source_url = "https://imgflip.com/memetemplates"
+    fetched_templates = []
+    errors = 0
+    for i in tqdm(range(args.from_page, args.pages + 1)):
+        print(f"Requesting: {args.source_url}?page={i}")
+        response = requests.get(f"{args.source_url}?page={i}")
+
+        print(response)
+
+        if response.status_code != 200:
+            print("Bad response")
+            break
+        
+        body = BeautifulSoup(response.text, 'html.parser')
+        templates = body.findAll("div", {"class": "mt-box"})
+        
+        print(len(templates))
+
+        for template in templates:
+            try:
+                template_url = "https://"+template.find('img', {"class": "shadow"})['src'][2:]
+                template_id, template_format = os.path.splitext(template_url.split("/")[-1])
+
+                template_title = template.find("h3", {"class": "mt-title"}).find("a")
+                template_title = "" if template_title is None else template_title.text
+
+                template_data = {
+                    "id": template_id,
+                    "format": template_format,
+                    "website": "imgflip",
+                    "url": template_url,
+                    "title": template_title
+                }
+                fetched_templates.append(template_data)
+            except:
+                errors += 1
+            # time.sleep(args.delay)
+
+    print(f"Fetched: {len(fetched_templates)} templates. Found {errors} error(s).")
+    return fetched_templates
+
+
+def process_imgflip(args):
+    '''
+    https://gist.github.com/WalterSimoncini/defca6de456bb168ada303085358bf0a
+    '''
     fetched_memes = []
     errors = 0
     for i in tqdm(range(args.from_page, args.pages + 1)):
@@ -28,7 +81,7 @@ def process_category(args):
                 continue  # NSFW memes are available only to logged in users
             
             try:
-                meme_url = meme.find("img", {"class": "base-img"})["src"][2:]
+                meme_url = 'https://'+meme.find("img", {"class": "base-img"})["src"][2:]
                 meme_id, meme_format = os.path.splitext(meme_url.split("/")[-1])
 
                 # Handle anonymous authors
@@ -46,7 +99,7 @@ def process_category(args):
                     "id": meme_id,
                     "format": meme_format,
                     "website": "imgflip",
-                    "url": 'https://'+meme_url,
+                    "url": meme_url,
                     "author": meme_author,
                     "title": meme_title,
                     "text": meme_text.lower()
@@ -66,9 +119,9 @@ if __name__ == '__main__':
     # ap.add_argument("--source_url", default="https://imgflip.com/tag/programming", help="Memes list url (e.g. https://imgflip.com/meme/Bird-Box)", type=str)
     ap.add_argument("--tag", required=True, type=str)#default=['programming', 'artificial intelligence', 'computer'], type=list)
     ap.add_argument("--from_page", default=1, help="Initial page", type=int)
-    ap.add_argument("--pages", default=10, help="Maximum page number to be scraped", type=int)
+    ap.add_argument("--pages", default=44, help="Maximum page number to be scraped", type=int)
     ap.add_argument("--delay", default=2, help="Delay between page loads (seconds)", type=int)
-    ap.add_argument("-o", "--output", default="data.tsv")
+    ap.add_argument("-o", "--output", default="templates.tsv")
     args = ap.parse_args()
 
     # category = args.source_url.split("/")[-1].replace("-", " ")
@@ -79,9 +132,14 @@ if __name__ == '__main__':
     print(f"Processing tag: {args.tag}")
     
     # Get the data
-    args.source_url = f"https://imgflip.com/tag/{args.tag.replace(' ', '+')}"
-    data = process_category(args)
+    # args.source_url = f"https://imgflip.com/tag/{args.tag.replace(' ', '+')}"
+    # data = process_imgflip(args)
     
+    # args.source_url = f"https://ww.9gag.com/search/?query={args.tag.replace(' ', '+')}"
+    # data = process_9gag(args)
+    
+    data = templates_imgflip(args)
+
     # Create a pd.DataFrame and save (append to existing .tsv)
     df = pd.DataFrame(data)
     print(df.head(20))
