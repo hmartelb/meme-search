cb0fdf8757373ecf2319d8d6d3badcd251aa355a
Héctor Martel <hmartelb@hotmail.com>
2021-05-07 11:50:24 +0100

    Update wsgi.py

--- frontend/wsgi.py
+++ frontend/wsgi.py
@@ -5,5 +5,6 @@ import waitress
 if __name__ == "__main__":
     # app.run(port=7255, debug=False)
     # logger = logging.getLogger('waitress')
-    # logger.setLevel(logging.INFO)
+    # logger.setLevel(logging.INFO) 
+    print(f"Serving on 0.0.0.0:5006 (Press CTRL+C to quit)")
     waitress.serve(app, listen='0.0.0.0:5006', url_scheme='https')
