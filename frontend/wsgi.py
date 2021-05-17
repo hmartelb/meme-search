e342118a31d389851f5f9612a4557a94ffc97061
Héctor Martel <hmartelb@hotmail.com>
2021-05-17 12:30:21 +0100

    Update wsgi.py
    Fixed task queue depth warning. 6 threads instead of 4

--- frontend/wsgi.py
+++ frontend/wsgi.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     # logger = logging.getLogger('waitress')
     # logger.setLevel(logging.INFO) 
     print(f"Serving on 0.0.0.0:5006 (Press CTRL+C to quit)")
-    waitress.serve(app, listen='0.0.0.0:5006', url_scheme='https')
+    waitress.serve(app, listen='0.0.0.0:5006', url_scheme='https', threads=6)
